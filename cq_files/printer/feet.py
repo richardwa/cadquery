75048975aab08712df216157dabe42936ef253e6
Richard Wang <rwang83@gmail.com>
2021-05-10 03:05:46 -0400

    update

---
+++ cq_files/printer/feet.py
@@ -0,0 +1,14 @@
+import cadquery as cq
+from cqextension.monkey_patch import seamSelector
+from slicer.slice import setSlicerSettings
+
+p = cq.Workplane().box(24, 24, 8).edges("|Z").fillet(2)
+s = p.faces(">Z").rect(20.5, 20.5).extrude(-2, combine=False)
+p = p.cut(s)
+cboreDepth = 4
+p = p.faces("<Z").workplane().cboreHole(5.5, 12, cboreDepth)
+
+# add bridge layer
+p = p.faces("<Z").workplane(offset=-cboreDepth).box(13, 13, .2)
+show_object(p)
+setSlicerSettings(p, duplicate=4)
